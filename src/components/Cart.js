6849e6bf4a6f9566784626881e7a96ff6f6eeefa
Muhammad-Abdullah-Arslan-Shah <154911877+Muhammad-Abdullah-Arslan-Shah@users.noreply.github.com>
2024-04-04 15:20:22 +0500

    Add remove Item functionality in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,17 +2,24 @@ import React, { useContext } from "react";
 import FoodContext from "../context/FoodContext";
 
 function Cart() {
-  const { cartItems } = useContext(FoodContext);
+  const { cartItems,setCartItems } = useContext(FoodContext);
 
   // Calculate total price of all items in the cart
   const totalAmount = cartItems.reduce((total, item) => total + (item.price * item.quantity), 0);
 
+  // Function to handle removal of items from the cart
+  const handleRemoveFromCart = (indexToRemove) => {
+    const newCartItems = cartItems.filter((item, index) => index !== indexToRemove);
+    setCartItems(newCartItems);
+  };
+
   return (
     <div className="container">
-      <h1 className="font-effect-fire text-center my-5" >Checkout</h1>
-      {cartItems.length === 0 ? (<div> 
-        <h1 className="text-light">Your cart is empty.</h1>
-        <p className="text-light"> Please Select Food items from Menu</p>
+      <h1 className="font-effect-fire text-center my-5">Checkout</h1>
+      {cartItems.length === 0 ? (
+        <div>
+          <h1 className="text-light">Your cart is empty.</h1>
+          <p className="text-light">Please Select Food items from Menu</p>
         </div>
       ) : (
         <>
@@ -21,17 +28,19 @@ function Cart() {
               <div key={index} className="col-md-4 mb-3">
                 <div className="card bg-warning text-dark">
                   <div className="card-body">
+                   
                     <h5 className="card-title">{item.title}</h5>
                     <p className="card-text">Quantity: {item.quantity}</p>
                     <p className="card-text">Price: Rs.{item.price * item.quantity}</p>
                     <p className="card-text">Special instruction: {item.specialInstructions}</p>
+                    <button className="btn btn-sm btn-danger float-left" onClick={() => handleRemoveFromCart(index)}>Remove</button>
                   </div>
                 </div>
               </div>
             ))}
           </div>
           <h3 className="mt-3 text-light">Total Amount: Rs.{totalAmount}</h3>
-          <button className="btn btn-warning m-2">Confirm Order</button>
+          <button className="btn btn-danger m-2">Confirm Order</button>
         </>
       )}
     </div>
